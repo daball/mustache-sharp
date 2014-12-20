b7f7e09a629dca9266bc412fe1012779e028ed88
David Ball <david@daball.me>
2014-12-20 04:30:39 -0500

    Added DynamicObject test facility
    DynamicObject support in PropertyDictionary effectively works as this
    test passes.

---
+++ mustache-sharp.test/DynamicObjectTester.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mustache;
+
+namespace Mustache.Test
+{
+    /// <summary>
+    /// Summary description for DynamicObjectTester
+    /// </summary>
+    [TestClass]
+    public class DynamicObjectTester
+    {
+        public DynamicObjectTester()
+        {
+            //set desired test output without using Mustache
+            this.desiredTestOutput = String.Format(testStringFormatTemplate, new object[] {
+                testDynamicObject.Name,
+                testDynamicObject.Tags[0],
+                testDynamicObject.Tags[1],
+                testDynamicObject.Address.Address1,
+                testDynamicObject.Address.City
+            });
+        }
+
+        private TestContext testContextInstance;
+
+        private string testMustacheTemplate = "{{Name}} {{#each Tags}}{{this}} {{/each}}{{#with Address}}{{Address1}} {{City}}{{/with}}";
+        private string testStringFormatTemplate = "{0} {1} {2} {3} {4}";
+        private dynamic testDynamicObject = new {
+                                                Name = "My Name",
+                                                Tags = new string[]
+                                                {
+                                                    "Tag 1",
+                                                    "Tag 2"
+                                                },
+                                                Address = new
+                                                {
+                                                    Address1 = "101 One Way",
+                                                    City = "My City"
+                                                }
+                                            };
+        private string desiredTestOutput = String.Empty;
+
+        private FormatCompiler compiler = new FormatCompiler();
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestDynamicObjectRender()
+        {
+            Assert.AreEqual(desiredTestOutput, compiler.Compile(testMustacheTemplate).Render(testDynamicObject));
+        }
+    }
+}
